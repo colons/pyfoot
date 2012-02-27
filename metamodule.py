88f4c653b3560d44c6deba98c5224ccd333b6c35
Iain Dawson <iain@musicfortheblind.co.uk>
2012-02-27 20:05:09 +0000

    MetaModule.translate() now raises an error when bing freaks out about languages.

--- metamodule.py
+++ metamodule.py
@@ -13,5 +13,7 @@ class MetaModule:
         query = urllib.quote(phrase)
         page = urllib.urlopen('http://api.microsofttranslator.com/V2/Ajax.svc/Translate?appId=%s&from=%s&to=%s&text="%s"' % (self.conf.get('bing_app_id'), source, target, query))
         result = page.read()
+        if result[4:-1].startswith('ArgumentOutOfRangeException: '):
+            raise NameError("that's not a language, silly")
         print result[4:-1]
         return result[4:-1]
