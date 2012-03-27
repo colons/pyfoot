26f5339ce2cd1472a042b92e3d2d5eb338573102
Joseph Cordella <ihave@domain.com>
2012-03-26 19:24:01 -0500

    Corrected formatting on !help line. (pedantic changes)

--- modules/help.py
+++ modules/help.py
@@ -6,4 +6,4 @@ class Module(metamodule.MetaModule):
         """ HALP """
         help = parser.args(message.content, 'help', self.conf)
         if help:
-            self.irc.send(message.source, '\x02features\x02 \x034:\x03 http://woof.bldm.us/help/ \x034|\x03 \x02code\x02\x034 :\x03 https://bitbucket.org/colons/pyfoot/ \x034|\x03 \x02bug?\x02\x034 :\x03 https://bitbucket.org/colons/pyfoot/issues/new')
+            self.irc.send(message.source, '\x02features\x02\x034 :\x03 http://woof.bldm.us/help/ \x034|\x03 \x02code\x02\x034 :\x03 https://bitbucket.org/colons/pyfoot/\x034 |\x03 \x02bug?\x02\x034 :\x03 https://bitbucket.org/colons/pyfoot/issues/new')
