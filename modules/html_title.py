f9a6904275eb42bf4f25c588eb51deea8a2388a6
Ensign Tropic <ensnrc@gmx.com>
2012-04-25 07:31:23 -0500

    Pulled out user_agents to make a common module, http_helper

--- modules/html_title.py
+++ modules/html_title.py
@@ -2,21 +2,13 @@ import BeautifulSoup
 import urllib
 from urlparse import urlparse
 import string
+import http_helper
 from random import choice
 import re
 
 import metamodule
 
 class Module(metamodule.MetaModule):
-    user_agents = [
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11',
-        'Opera/9.25 (Windows NT 5.1; U; en)',
-        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
-        'Mozilla/5.0 (compatible; Konqueror/3.5; Linux) KHTML/3.5.5 (like Gecko) (Kubuntu)',
-        'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.0.12) Gecko/20070731 Ubuntu/dapper-security Firefox/1.5.0.12',
-        'Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/1.2.9'
-    ]
-
     def act(self, message):
         for word in message.content.split():
             if word.startswith('http://') or word.startswith('https://'):
@@ -40,12 +32,11 @@ class Module(metamodule.MetaModule):
                             join = '?'
                         url_fragment = urllib.quote(word[hashbang_index+2:], '=')
                         word = url_base + join + '_escaped_fragment_=' + url_fragment
-                        print word
 
                     parsed_url = urlparse(word)
 
                     opener = urllib.FancyURLopener()
-                    setattr(opener, 'version', choice(self.user_agents))
+                    setattr(opener, 'version', choice(http_helper.user_agents))
 
                     try:
                         pagesoup = BeautifulSoup.BeautifulSoup(opener.open(word))
