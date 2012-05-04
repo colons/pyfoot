c3f094b535ebd62a71cb8452ba5095ccf47b621c
Iain Dawson <iain@bldm.us>
2012-05-04 10:24:49 +0100

    rantext, docs changes.

---
+++ modules/rantext.py
@@ -0,0 +1,42 @@
+from random import choice
+
+import module
+
+
+class Module(module.Module):
+    """ Retrieve a random line from a text file. Can be directed at individuals. """
+    def prepare(self):
+        self.sources = {}
+        for source in self.conf.get('rantext_sources'):
+            filename = self.conf.get('content_dir')+source+'.txt'
+            file = open(filename)
+            line_list = []
+            for line in file:
+                line_list.append(line)
+            self.sources[source] = line_list
+
+
+    def register_commands(self):
+        self.commands = []
+        for source in self.conf.get('rantext_sources'):
+            everyone_func = lambda message, args: self.do_a_thing(message, source, args)
+            setattr(everyone_func, '__doc__', '$<comchar>%s\n>%s' % (source, self.extract(source)))
+
+            targetted_func = lambda message, args: self.do_a_thing(message, source, args)
+
+            self.commands.append((source, everyone_func))
+            self.commands.append(('%s <nick>' % source, targetted_func))
+
+        print self.commands
+    
+    def do_a_thing(self, message, source, args):
+        line = self.extract(source)
+
+        if 'nick' in args:
+            line = '%s: %s' % (args['nick'], line)
+
+        self.irc.send(message.source, line)
+    
+    def extract(self, source):
+        """ Opens file and returns a random item from it """
+        return choice(self.sources[source])
