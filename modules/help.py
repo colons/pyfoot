361ac08a30f4a8165517508c325698436d08be67
Iain Dawson <iain@bldm.us>
2012-05-05 18:26:06 +0100

    Module-specific help messages.

--- modules/help.py
+++ modules/help.py
@@ -3,10 +3,17 @@ import module
 class Module(module.Module):
     def register_commands(self):
         self.commands = [
-                ('help', self.send_help),
+                ('help', self.all_help),
+                ('help <module>', self.targetted_help),
                 ]
 
-    def send_help(self, message, args):
+    def targetted_help(self, message, args):
+        if args['module'] in self.conf.get('modules'):
+            self.irc.send(message.source, '\x02%s\x02 | http://woof.bldm.us/help/%s#%s' % (args['module'], self.conf.alias, args['module']), pretty=True)
+        else:
+            self.irc.send(message.source, '\x02%s\x02 | no such module', pretty=True)
+
+    def all_help(self, message, args):
         """ Returns links to this page and to pyfoot's <a href="https://bitbucket.org/colons/pyfoot/">source code</a> and <a href="https://bitbucket.org/colons/pyfoot/issues/new">issue tracker</a>.
         $<comchar>help
         >\x02features\x02\x034 :\x03 http://woof.bldm.us/help/<network>\x034 |\x03\x02 code\x02\x034 :\x03 https://bitbucket.org/colons/pyfoot\x034 |\x03\x02 bug?\x02\x034 :\x03 https://bitbucket.org/colons/pyfoot/issues/new
