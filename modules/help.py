cf71c995e41f040e730760d58838c884a994481b
Iain Dawson <iain@bldm.us>
2012-05-06 01:21:40 +0100

    Made docs require universally trailing slash.

--- modules/help.py
+++ modules/help.py
@@ -9,13 +9,13 @@ class Module(module.Module):
 
     def targetted_help(self, message, args):
         if args['module'] in self.conf.get('modules'):
-            self.irc.send(message.source, '\x02%s\x02 | http://woof.bldm.us/help/%s#%s' % (args['module'], self.conf.alias, args['module']), pretty=True)
+            self.irc.send(message.source, '\x02%s\x02 | http://woof.bldm.us/help/%s/#%s' % (args['module'], self.conf.alias, args['module']), pretty=True)
         else:
             self.irc.send(message.source, '\x02%s\x02 | no such module' % args['module'], pretty=True)
 
     def all_help(self, message, args):
         """ Returns links to this page and to pyfoot's <a href="https://bitbucket.org/colons/pyfoot/">source code</a> and <a href="https://bitbucket.org/colons/pyfoot/issues/new">issue tracker</a>.
         $<comchar>help
-        >\x02features\x02\x034 :\x03 http://woof.bldm.us/help/<network>\x034 |\x03\x02 code\x02\x034 :\x03 https://bitbucket.org/colons/pyfoot\x034 |\x03\x02 bug?\x02\x034 :\x03 https://bitbucket.org/colons/pyfoot/issues/new
+        >\x02features\x02\x034 :\x03 http://woof.bldm.us/help/<network>/\x034 |\x03\x02 code\x02\x034 :\x03 https://bitbucket.org/colons/pyfoot\x034 |\x03\x02 bug?\x02\x034 :\x03 https://bitbucket.org/colons/pyfoot/issues/new
         """
         self.irc.send(message.source, '\x02features\x02\x034 :\x03 http://woof.bldm.us/help/%s/\x034 |\x03\x02 code\x02\x034 :\x03 https://bitbucket.org/colons/pyfoot\x034 |\x03\x02 bug?\x02\x034 :\x03 https://bitbucket.org/colons/pyfoot/issues/new' % self.conf.alias)
