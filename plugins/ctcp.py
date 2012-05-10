457996dc930bd6c5306f3dd67b2539eda3c71a6e
Iain Dawson <iain@bldm.us>
2012-05-10 17:10:42 +0100

    ♪┌|°з°|┘♪ └|°ε°|┐♪ ┌|°з°|┘♪

---
+++ plugins/ctcp.py
@@ -0,0 +1,11 @@
+import plugin
+
+class Plugin(plugin.Plugin):
+    def register_commands(self):
+        self.regexes = [
+                ('\x01VERSION\x01', self.ctcp),
+                ]
+
+    def ctcp(self, message, args):
+        """ Get version information. """
+        self.irc.ctcp(message.source, 'VERSION', 'pyfoot; see %s' % self.conf.get('web_url'), notice=True)
