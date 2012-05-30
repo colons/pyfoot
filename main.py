d5c5cd6b1871db8f5ad52470cc98e2bfa97fff46
Ensign Tropic <ensnrc@gmx.com>
2012-05-30 03:13:19 -0500

    General purpose improvements below.
    Working log and pid files.
    
    New Printer class for flexible tee-style logging.
    
    Cleaner choice logic in the main file.
    
    Fixed not expanding '~' in a user's custom 'content-dir'.

--- main.py
+++ main.py
@@ -9,92 +9,91 @@ __url__ = 'https://github.com/colons/pyfoot'
 from conf import Config
 from irc import IRC
 from network import Network
+from printer import Printer
 import argparse
 import signal
+import sys
+import os
 
 def parse_args():
     """ Set up, and return the results of, the argument parser. """
-    parser = argparse.ArgumentParser(description=__desc__, add_help=False, prog=__title__)
-    required = parser.add_argument_group(title='Required')
-    optional = parser.add_argument_group(title='Optional')
+    parser = argparse.ArgumentParser(add_help=False, prog=__title__, usage='%(prog)s [options] [logging] network')
+    required = parser.add_argument_group(title='required')
+    optional = parser.add_argument_group(title='options')
+    log = parser.add_argument_group(title='logging')
 
     required.add_argument('network', help='an IRC network profile in your configuration')
-    optional.add_argument('--help', '-h', action='help', help='print this help and exit')
-    optional.add_argument('--version', '-v', action='version', version='%(prog)s '+__version__, help="show %(prog)s's version and exit")
-    optional.add_argument('--config', '-c', dest='config', default=None, help='specify an alternative configuration')
-    optional.add_argument('--daemon', '-d', dest='start', action='store_const', const=start_daemon, default=start_normal, help='start %(prog)s in daemon mode')
+
+    optional.add_argument('-h', '--help', action='help', help='print this help and exit')
+    optional.add_argument('-v', '--version', action='version', version='%(prog)s '+__version__, help="show %(prog)s's version and exit")
+    optional.add_argument('-c', '--config', dest='config', default=None, help='specify an alternative configuration')
+    optional.add_argument('-d', '--daemon', dest='daemonise', action='store_true', help='start %(prog)s in daemon mode')
+    optional.add_argument('-p', '--pid', dest='pidfile', metavar='FILE', default=None, help='keep a pid file')
+
+    log.add_argument('-o', '--log', metavar='[FILE]', dest='logfile', default=None, help="log in FILE")
+    log.add_argument('-a', '--append', dest='logappend', action='store_true', help="append to FILE when logging")
     return parser.parse_args()
 
-# Thank you http://code.activestate.com/recipes/278731-creating-a-daemon-the-python-way/
+def kill_handler(signum, frame):
+    if signum == signal.SIGINT:
+        sig = 'SIGINT'
+    elif signum == signal.SIGTERM:
+        sig = 'SIGTERM'
+    print('\n !! ' + sig + ' caught : shutting down')
+    # Delete the pid file on a clean exit
+    if args.pidfile:
+        os.remove(args.pidfile)
+    irc.quit()
+
+def start_normal():
+    """ Start pyfoot normally. """
+    sys.stderr.write(' @@ my process id is ' + str(os.getpid()) + '\n')
+
+# Derived from http://code.activestate.com/recipes/278731-creating-a-daemon-the-python-way/
 def start_daemon():
     """ Start pyfoot in daemon mode. """
-    import os
-
-    # Constants
-    if (hasattr(os, 'devnull')):
-        REDIRECT_TO  = os.devnull
-    else:
-        REDIRECT_TO = '/dev/null'
-    MAXFD = 1024
 
     pid = os.fork()  # First child
     if pid == 0:
         os.setsid()
         pid = os.fork()  # Second child
         if pid != 0:
-            print('now running in the background : my process id is ' + str(pid))
+            sys.stderr.write('now running in the background : my process id is ' + str(pid) + '\n')
             os._exit(0)
     else:
         os._exit(0)
 
-    # Check maximum file descriptor or set to default
-    import resource
-    maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
-    if (maxfd == resource.RLIM_INFINITY):
-        maxfd = MAXFD
+if __name__ == '__main__':
+    # Parse the command line arguments
+    # A --help/-h or --version/-v will end the program here
+    args = parse_args()
 
-    # Iterate through and close all file descriptors.
-    for fd in range(0, maxfd):
-        try:
-            os.close(fd)
-        except OSError: # ERROR, fd wasn't open to begin with (ignored)
-            pass
+    # Print the pid, and fork to background if requested
+    if args.daemonise == True:
+        start_daemon()
+    else:
+        start_normal()
 
-    # This call to open is guaranteed to return the lowest file descriptor,
-    # which will be 0 (stdin), since it was closed above.
-    os.open(REDIRECT_TO, os.O_RDWR)  # standard input (0)
+    # Register the TERM and INT signal handler
+    signal.signal(signal.SIGTERM, kill_handler)
+    signal.signal(signal.SIGINT, kill_handler)
 
-    # Duplicate standard input to standard output and standard error.
-    os.dup2(0, 1)  # standard output (1)
-    os.dup2(0, 2)  # standard error (2)
+    # Write out the pid file if requested
+    if args.pidfile:
+        pidfile = open(args.pidfile, 'wt', encoding='us-ascii')
+        pidfile.write(str(os.getpid()))
+        pidfile.close()
 
-    start_normal(False)
+    # Override sys.stdout with the custom Printer class
+    sys.stdout = Printer(args.logfile, args.logappend, args.daemonise)
 
-def start_normal(nodaemon=True):
-    """ Start pyfoot. """
-    if nodaemon:
-        from os import getpid
-        print(' -- my process id is ' + str(getpid()))
-
+    # Create pyfoot's custom objects
     conf = Config(args.network, args.config)
     irc = IRC(conf)
     network = Network(conf, irc)
 
-    def kill_handler(signum, frame):
-        if signum == signal.SIGINT:
-            sig = 'SIGINT'
-        elif signum == signal.SIGTERM:
-            sig = 'SIGTERM'
-        print('\n !! ' + sig + ' caught : shutting down')
-        irc.quit()
-
-    signal.signal(signal.SIGTERM, kill_handler)
-    signal.signal(signal.SIGINT, kill_handler)
-
+    # Start the main event loop
     while True:
         data = irc.listen()
         network.dispatch(data)
 
-if __name__ == '__main__':
-    args = parse_args()
-    args.start()
